62895215940bee8680d21a9c2e71d5e92f64467a
Jan Bassen <bassen@numos.xyz>
2025-02-20 16:10:27 +0100

    :bug: fix time difference node logic

--- packages/shared/src/engine/nodes/time-difference/logic.ts
+++ packages/shared/src/engine/nodes/time-difference/logic.ts
@@ -7,36 +7,18 @@ export const timeDifferenceLogic: NodeLogic<TimeDifferenceNode> = {
     output: async ({ getInputValue, getControlValue }) => {
       const time1 = await getInputValue('time1')
       const time2 = await getInputValue('time2')
+      const unit = getControlValue('unit').value as
+        | 'seconds'
+        | 'minutes'
+        | 'hours'
+        | 'days'
+        | 'weeks'
+        | 'months'
+        | 'years'
       const start = DateTime.fromMillis(time1.value)
       const end = DateTime.fromMillis(time2.value)
-      const diff = end.diff(start)
-      const unit = getControlValue('unit').value
-      let value: number
-      switch (unit) {
-        case 'seconds':
-          value = diff.seconds
-          break
-        case 'minutes':
-          value = diff.minutes
-          break
-        case 'hours':
-          value = diff.hours
-          break
-        case 'days':
-          value = diff.days
-          break
-        case 'weeks':
-          value = diff.weeks
-          break
-        case 'months':
-          value = diff.months
-          break
-        case 'years':
-          value = diff.years
-          break
-        default:
-          throw new Error(`Unknown unit ${unit}`)
-      }
+      const diff = end.diff(start, unit)
+      const value = Math.abs(diff[unit])
       return { type: 'number', format: 'single', value }
     },
   },
